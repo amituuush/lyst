30204b9a5bccbc6c269fc732b5e08e38606e48c2
Amit Ranan <ranan.amit@gmail.com>
2016-02-26 17:56:37 -0800

    index.js change

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 var express = require('express');
 var app = express();
 
+app.set('port', (process.env.PORT || 5000));
+
 app.use(express.static('public'));
 
 app.get('/', function(request, response) {
     response.send('app working');
 
 });
-app.listen(3000, function() {
+app.listen(app.get('port'), function() {
     console.log('Running on local host 3000');
 });
